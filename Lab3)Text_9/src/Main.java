c3e8d1e68c21ea0cc22b38de04df13fa034d0eea
Igor <fpm.shapiro@bsu.by>
2019-09-29 18:51:07 +0300

    Lab3

---
+++ Lab3)Text_9/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] alph = new int['z' - 'a' + 1];
+        Scanner in = new Scanner(System.in);
+        String str;
+
+        System.out.println("Input lines: ");
+	    do {
+            str = in.nextLine();
+            str = str.toLowerCase();
+
+            for (int i = 0; i < str.length(); i++) {
+                if ('a' <= str.charAt(i) && str.charAt(i) <= 'z'){
+                    alph[str.charAt(i) - 'a']++;
+                }
+            }
+        } while (!str.equals(""));
+
+	    System.out.print("Searched letters: ");
+        for (int i = 0; i < alph.length; i++) {
+            if (alph[i] != 0) {
+                System.out.print((char)(i + 'a') + " ");
+            }
+        }
+
+    }
+}
